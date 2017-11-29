0ce3e3892e3c7124ac68cc51db03022766845df3
wagnersperling <wagnersperling@gmail.com>
2017-11-29 09:26:55 -0200

    retirado comentario e corrigido sintaxe

--- src/config/server.js
+++ src/config/server.js
@@ -6,11 +6,12 @@ const server = express()
 const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended: true}))
-//sever.use(bodyParser.json())
+server.use(bodyParser.json())
 server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`)
+    
 })
 
 
